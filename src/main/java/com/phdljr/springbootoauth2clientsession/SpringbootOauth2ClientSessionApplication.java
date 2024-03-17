243eccbfe93f64c9849935a2cec32b782316708d
phdljr <phdljr@naver.com>
2024-03-17 18:12:45 +0900

    init

---
+++ src/main/java/com/phdljr/springbootoauth2clientsession/SpringbootOauth2ClientSessionApplication.java
@@ -0,0 +1,13 @@
+package com.phdljr.springbootoauth2clientsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2ClientSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2ClientSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phdljr/springbootoauth2clientsession/SpringbootOauth2ClientSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phdljr.springbootoauth2clientsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2ClientSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
